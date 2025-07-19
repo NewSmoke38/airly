889b102f54c4d327ef0bf0fa37fa6540197198a5
Shivani <shivanialive2007@gmail.com>
2025-07-19 21:56:57 +0530

    refactor: remove test routes, update package.json for new entry point

--- Backend/src/routes/test.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { testBackend } from "../controllers/test.controller.js";
-
-const router = Router();
-
-// Test route for backend health check
-router.route("/").get(testBackend);
-
-export default router; 
