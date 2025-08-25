b90d792e8b1ce8b5b3a6a71be51ff16fd3fe32bc
Shivani <shivanialive2007@gmail.com>
2025-08-25 23:33:07 +0530

    add render url

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const baseURL = 'https://airly-backend-production.up.railway.app/api/v1';
+let Prod = true;
+
+const ProdBaseURL = 'https://vibely-3q1i.onrender.com';
+const LocalBaseURL = 'https://vibely-3q1i.onrender.com/api/v1';
+
+const baseURL = Prod ? ProdBaseURL : LocalBaseURL;
 
 const axiosInstance = axios.create({
   baseURL,
